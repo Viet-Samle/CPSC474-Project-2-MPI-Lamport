b65dfe45aa73713c7c71d64c62465be07315cd68
Viet-Samle <viet.sam.n.le@gmail.com>
2018-12-04 14:20:07 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
 #include <algorithm>
+#include <fstream>
 #include "mpi.h"
 
 using namespace std;
 
 #define N 10
 #define M 10
+#define event_size 2
 
 int main(int argc, char *argv[]) {
   int size, rank;
-  string given_events[N][M];
+  char given_events[N][M][event_size];
 
   int send_array[100];
   fill_n(send_array, 100, -1);
 
-  MPI_Init( &argc, &argv );
-  MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-  MPI_Comm_size( MPI_COMM_WORLD, &size );
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
   if (rank == 0) {
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < M; j++) {
-          given_events[i][j] = "NULL";
-        }
-      }
+    string line;
+    ifstream test_file;
+    if (test_file.open("input.txt")) {
+
+    }
 
     cout << "Enter the input for the logical clock" << endl;
     for (int i = 0; i < N; i++) {
